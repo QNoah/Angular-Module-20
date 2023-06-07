0a560ba8b35c270d64095e5f87c49aa0033c0482
Quinten <90308786+QNoah@users.noreply.github.com>
2023-06-07 11:18:06 +0200

    Media component toegevoegt

---
+++ Angular LinkedIn Learning/training/src/app/media-item/media-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.scss']
+})
+export class MediaItemComponent {
+
+}
